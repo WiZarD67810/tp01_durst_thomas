a7ba2c85ab56bdc076eefc9e3de30b3622823739
Thomas DURST <thomas.durst67@gmail.com>
2021-09-10 14:57:55 +0200

    feat(): TP01

---
+++ script/script.js
@@ -0,0 +1,44 @@
+/*(function () {
+    
+    var forms = document.querySelector(".needs-validation")
+
+    Array.prototype.slice.call(forms).forEach(function (form) {
+        form.addEventListener("submit", function (event) {
+            // Si les champs sont pas vide on stop
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            } 
+
+            form.classList.add("was-validated")
+
+        }, false)
+    });
+})()*/
+
+function onChecking(event) {
+
+    let form = document.querySelector(".needs-validation");
+
+    if(!form.checkValidity()) {
+        form.classList.add("was-validated");
+
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    let pwd1 = document.getElementById("password").value;
+    let pwd2 = document.getElementById("password2").value;
+
+    if (pwd1 != pwd2) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        form.classList.add("was-validated");
+        document.getElementById("password2").setCustomValidity(":invalid");
+    } else {
+        alert("Le formulaire a bien été envoyé !");
+
+        return true
+    }
+}
